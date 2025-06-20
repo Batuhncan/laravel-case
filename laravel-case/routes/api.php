e7f6061ec6ffa2b284105da282aebaa82da9b477
Batuhan CAN <batu_2000_15@hotmail.com>
2025-06-20 18:37:45 +0300

    Veriler için controller ve model migration oluşturulması part2

--- laravel-case/routes/api.php
+++ laravel-case/routes/api.php
@@ -2,4 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LocationController;
 
+route::controller(LocationController::class)->group(function () {
+    
+});
